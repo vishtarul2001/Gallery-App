185457ba92da7c5a443899878ea8640c681a5f08
vishtarul2001 <sharmavishwash2001@gmail.com>
2023-10-24 21:30:38 +0530

    first commit

---
+++ src/Pages/SinglePhoto.jsx
@@ -0,0 +1,30 @@
+import React, { useEffect, useState } from "react";
+import "../style/SinglePhoto.css"
+import { useParams } from "react-router-dom";
+
+export const SinglePhoto = () => {
+    const[data,setData]=useState();
+    const {id} = useParams()
+    const getData=async()=>{
+      const resp=await fetch(`https://api.slingacademy.com/v1/sample-data/photos/${id}`);
+      const {photo}=await resp.json();
+      setData(photo)
+    }
+  
+    useEffect(()=>{
+     getData();
+    })
+    return (
+        <div id="singlePhoto">
+                <img src={(data)?data.url:''} alt="" />
+                
+                <div>
+                <h1>{(data)?data.title:''}</h1>
+                    <p>
+                            {(data)?data.description:''}
+                    </p>
+                    </div>
+              
+        </div>
+    )
+}
